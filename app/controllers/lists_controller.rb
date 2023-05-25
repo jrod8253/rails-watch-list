f9fdc44641e920480d14babac31e171572d8d87c
Jared Champayne <j4champa@gmail.com>
2023-05-25 21:06:52 +0200

    functional without any style

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,35 @@
 class ListsController < ApplicationController
+  def index
+    @list = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_paramas)
+
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path, status: :see_other
+  end
+
+  private
+
+  def list_paramas
+    params.require(:list).permit(:name)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require "json"
+require "open-uri"
+
+movie.delete.all
+url = "https://tmdb.lewagon.com/movie/top_rated"
+# URI is a library in ruby to help read the url before parse
+response = URI.open(url).read
+# this makes it more readable
+movies_data = JSON.parse(response)
+movies = movies_data['results']
+
+movies.each do |movie_data|
+  title = movie_data['title']
+  overview = movie_data['overview']
+  rating = movie_data['vote_average']
+  poster_url_base = "https://image.tmdb.org/t/p/original"
+  poster_url = movie_data['poster_path']
+  movie = Movie.new(title: title, overview: overview, rating: rating, poster_url: "#{poster_url_base}#{poster_url}")
+  movie.save
+end
