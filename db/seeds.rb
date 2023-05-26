95a5d8914fe2b533b236d9fb99f398c8b484a4ff
Jared Champayne <j4champa@gmail.com>
2023-05-26 12:21:06 +0200

    updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
 require "json"
 require "open-uri"
 
-movie.delete.all
+# puts "Cleaning DB"
+# Movie.destroy_all
+# puts "DB now empty  "
+
 url = "https://tmdb.lewagon.com/movie/top_rated"
 # URI is a library in ruby to help read the url before parse
 response = URI.open(url).read
@@ -9,6 +12,7 @@ response = URI.open(url).read
 movies_data = JSON.parse(response)
 movies = movies_data['results']
 
+puts "Importing movies"
 movies.each do |movie_data|
   title = movie_data['title']
   overview = movie_data['overview']
@@ -18,3 +22,4 @@ movies.each do |movie_data|
   movie = Movie.new(title: title, overview: overview, rating: rating, poster_url: "#{poster_url_base}#{poster_url}")
   movie.save
 end
+puts "Movies have been created"
